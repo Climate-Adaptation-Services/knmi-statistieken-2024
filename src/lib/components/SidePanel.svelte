f233154e0c422ff3463e87cbd0f6bfce109f9ec2
stichtingcas <daniel@climateadaptationservices.com>
2024-05-23 18:59:12 +0200

    Update SidePanel.svelte

--- src/lib/components/SidePanel.svelte
+++ src/lib/components/SidePanel.svelte
@@ -30,33 +30,34 @@
   }
   
   let svgElement;
-  afterUpdate(() => {
-    select('.rough-sidepanel g').remove()
-    const rc = rough.svg(svgElement);
+  // afterUpdate(() => {
+  //   select('.rough-sidepanel g').remove()
+  //   const rc = rough.svg(svgElement);
 
-    // a 3 10 0 0 1 ${w*0.25} ${h*0.5}
-    // a 3 10 0 0 0 ${w*0.25} ${h*0.5}
-    const path = rc.path(
-      `M0 0 l${w} 0 l0 ${h}
+  //   // a 3 10 0 0 1 ${w*0.25} ${h*0.5}
+  //   // a 3 10 0 0 0 ${w*0.25} ${h*0.5}
+  //   const path = rc.path(
+  //     `M0 0 l${w} 0 l0 ${h}
 
-      l${-2*w} 0 z`,
-      { 
-        roughness: 0.3, 
-        fill: '#35575A', 
-        stroke: 'none',
-        fillStyle:'cross-hatch',
-        fillWeight: 1,
-        hachureGap: 2.2,
-        hachureAngle: 45,
-        strokeWidth: 4
-      });
-    svgElement.appendChild(path);
-  })
+  //     l${-2*w} 0 z`,
+  //     { 
+  //       roughness: 0.3, 
+  //       fill: '#35575A', 
+  //       stroke: 'none',
+  //       fillStyle:'cross-hatch',
+  //       fillWeight: 1,
+  //       hachureGap: 2.2,
+  //       hachureAngle: 45,
+  //       strokeWidth: 4
+  //     });
+  //   svgElement.appendChild(path);
+  // })
 </script>
 
 {#if w}
   <svg>
     <g class='rough-sidepanel' bind:this={svgElement}></g>
+      <rect width={w} height={h} fill='#35575A'/>
 
     <!-- <path fill='#35575A' style='filter: drop-shadow(rgb(0,0,0,0.4) 1rem 0.5rem 10px)'
       d="M0 0 l{w-50} 0
