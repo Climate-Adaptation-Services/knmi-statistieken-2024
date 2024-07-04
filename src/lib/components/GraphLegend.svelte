e01a9cc41fd043851a19d537fed19eaedf50b054
stichtingcas <daniel@climateadaptationservices.com>
2024-07-04 17:14:00 +0200

    Update GraphLegend.svelte

--- src/lib/components/GraphLegend.svelte
+++ src/lib/components/GraphLegend.svelte
@@ -22,6 +22,10 @@
     selectAll('.scenario-laag, .scenario-hoog').style('opacity', 1)
   }
 
+  function infoHover(){
+
+  }
+
 
 </script>
 
@@ -33,22 +37,26 @@
       <rect width='30' height='20' fill='red' x='150' y='-15' opacity='0.2'/>
       <line x1='150' y1='-5' x2='180' y2='-5' stroke='red'/>
     </g>
+    <image href='/images/info.png' width='16' y='-3' x='190' opacity='0.6' on:mouseover={() => infoHover()}/>
     <g transform='translate(0,40)' on:mouseover={() => mouseOverScenario('laag', 'hoog')} on:mouseout={() => mouseOutScenario()}>
       <text class='legend-text-laag' fill='#17A3D3' cursor='default'>Laagste scenario</text>
       <rect width='30' height='20' fill='#17A3D3' x='150' y='-15' opacity='0.2'/>
       <line x1='150' y1='-5' x2='180' y2='-5' stroke='#17A3D3'/>
     </g>
+    <image href='/images/info.png' width='16' y='27' x='190' opacity='0.6' on:mouseover={() => infoHover()}/>
     <g class='langjarig' style='fill:grey' transform='translate(-20,66)'>
       <circle r='4'/>
       <line x2={50} stroke='grey'/>
       <circle r='4' cx='50'/>
       <text font-size='12' x='65' y='0.32em'>Langjarig gemiddelde (30 jaar)</text>
     </g>
+    <image href='/images/info.png' width='16' y='57' x='214' opacity='0.6' on:mouseover={() => infoHover()}/>
     <g class='jaar-op-jaar' style='fill:grey' transform='translate(-20,100)'>
       <rect width='50' height='30' fill='grey' x='0' y='-15' opacity='0.2'/>
       <line x1='50' y1='-15' x2='50' y2='15' stroke='grey' stroke-dasharray='4 4' stroke-width='2'/>
       <text font-size='12' x='65' y='0.32em'>Jaar op jaar variatie (90% band)</text>
     </g>
+    <image href='/images/info.png' width='16' y='91.5' x='219' opacity='0.6' on:mouseover={() => infoHover()}/>
   </g>
   <!-- <h3>Toon scenario grafiek</h3>
   <div class='switch-div'> -->
