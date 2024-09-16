6d5e057d30185babb1ee4fbff7d4e3d4b592249f
stichtingcas <daniel@climateadaptationservices.com>
2024-09-16 15:36:36 +0200

    yes

--- src/lib/i18n/nl.js
+++ src/lib/i18n/nl.js
@@ -11,6 +11,50 @@ export const nederlands = {
   'Hitte':'Hitte',
   'Droogte':'Droogte',
   'Wateroverlast':'Wateroverlast',
-  'Zeespiegelstijging':'Zeespiegelstijging'
+  'Zeespiegelstijging':'Zeespiegelstijging',
+
+
+
+
+
+
+
+
+
+
+  'Aantal tropische dagen':'Aantal tropische dagen',
+  'Reeks droge dagen':'Reeks droge dagen',
+  'Reeks zomerse dagen':'Reeks zomerse dagen',
+  'Aantal tropische nachten':'Aantal tropische nachten',
+  'Aantal vorstdagen':'Aantal vorstdagen',
+  'Aantal warme dagen':'Aantal warme dagen',
+  'Aantal zomerse dagen':'Aantal zomerse dagen',
+  'Aantal ijsdagen':'Aantal ijsdagen',
+  'Aantal dagen ≥ 15 mm':'Aantal dagen ≥ 15 mm',
+  'Aantal dagen ≥ 25 mm':'Aantal dagen ≥ 25 mm',
+  'Jaarlijkse neerslag':'Jaarlijkse neerslag',
+  'Winterneerslag':'Winterneerslag',
+  'Zomerneerslag':'Zomerneerslag',
+  'Gemiddelde jaartemperatuur':'Gemiddelde jaartemperatuur',
+  'Gemiddelde wintertemperatuur':'Gemiddelde wintertemperatuur',
+  'Gemiddelde zomertemperatuur':'Gemiddelde zomertemperatuur',
+  'Koelgraaddagen':'Koelgraaddagen',
+  'Verwarmingsgraaddagen':'Verwarmingsgraaddagen',
+  'Lengte groeiseizoen':'Lengte groeiseizoen',
+  'Aantal extreem hete dagen':'Aantal extreem hete dagen',
+  'Jaarlijkse referentieverdamping':'Jaarlijkse referentieverdamping',
+  'Maximaal neerslagtekort':'Maximaal neerslagtekort',
+  '10-daagse neerslag - eens per jaar':'10-daagse neerslag - eens per jaar',
+  '10-daagse neerslag - eens per 10 jaar':'10-daagse neerslag - eens per 10 jaar',
+  '10-daagse neerslag - eens per 100 jaar':'10-daagse neerslag - eens per 100 jaar',
+  '10-daagse neerslag - eens per 1000 jaar':'10-daagse neerslag - eens per 1000 jaar',
+  'Dagneerslag - eens per jaar':'Dagneerslag - eens per jaar',
+  'Dagneerslag - eens per 10 jaar':'Dagneerslag - eens per 10 jaar',
+  'Dagneerslag - eens per 100 jaar':'Dagneerslag - eens per 100 jaar',
+  'Dagneerslag - eens per 1000 jaar':'Dagneerslag - eens per 1000 jaar',
+  'Uurneerslag - eens per jaar':'Uurneerslag - eens per jaar',
+  'Uurneerslag - eens per 10 jaar':'Uurneerslag - eens per 10 jaar',
+  'Uurneerslag - eens per 100 jaar':'Uurneerslag - eens per 100 jaar',
+  'Uurneerslag - eens per 1000 jaar':'Uurneerslag - eens per 1000 jaar',
 
 }
