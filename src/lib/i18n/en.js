7e9b62db692b5c03c8ad26d98c4f5400594b88de
stichtingcas <daniel@climateadaptationservices.com>
2024-09-17 09:58:09 +0200

    Update en.js

--- src/lib/i18n/en.js
+++ src/lib/i18n/en.js
@@ -24,7 +24,7 @@ export const english = {
   'Hoogste info':'The highest scenario presents the value for the scenario that shows the greatest change. This is the Hn or Hd scenario, depending on the indicator you are looking at. Below the figures, it indicates which scenario this is.',
   'Laagste info':'The lowest scenario presents the value for the scenario that shows the smallest change. This is the Ln or Ld scenario, depending on the indicator you are looking at. Below the figures, it indicates which scenario this is.',
   'gemiddelde info':'De punten in de grafiek tonen voor het huidige klimaat, rond 2050 en 2100 het gemiddelde over alle beschikbare jaren.',
-  'jaar op jaar info':'Door natuurlijke variatie zullen de waarden het ene jaar hoger en het andere jaar lager zijn dan de gemiddelde waarden weergeven met de punten. De figuur geeft daarom met lichtgekleurde blauwe en rode banden weer waarbinnen de 90% van de afzonderlijke jaarwaarden valt.',
+  'jaar op jaar info':'Due to natural variation, the values will be higher in some years and lower in others than the average values shown by the points. The figure therefore displays light-colored blue and red bands that indicate where 90% of the individual yearly values fall.',
 
 
   'Aantal tropische dagen':'Number of tropical days',
