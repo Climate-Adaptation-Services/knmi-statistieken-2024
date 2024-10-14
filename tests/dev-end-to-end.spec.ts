8ba14ed17577068565a22aca1cbc722852f8df22
stichtingcas <daniel@climateadaptationservices.com>
2024-10-14 12:41:40 +0200

    Update dev-end-to-end.spec.ts

--- tests/dev-end-to-end.spec.ts
+++ tests/dev-end-to-end.spec.ts
@@ -11,15 +11,15 @@ test('test', async ({ page }) => {
   let box = (await page.locator('.image-Droogte').boundingBox())!;
   await page.mouse.click(box.x + box.width / 2, box.y + box.height - 5);
   await page.locator('rect:nth-child(551)').click();
-  // await page.getByRole('textbox').first().click();
-  // await page.getByText('Maximaal neerslagtekort', { exact: true }).first().click();
+  await page.getByRole('textbox').first().click();
+  await page.getByText('Maximaal neerslagtekort', { exact: true }).first().click();
 
   box = (await page.locator('.image-Wateroverlast').boundingBox())!;
   await page.mouse.click(box.x + box.width / 2, box.y + box.height - 5);
-  // await page.getByRole('textbox').first().click();
-  // await page.getByText('Uurneerslag - eens per 1000 jaar', { exact: true }).first().click();
-  // await page.getByRole('textbox').nth(1).click();
-  // await page.getByText('2100 laagste scenario', { exact: true }).first().click();
+  await page.getByRole('textbox').first().click();
+  await page.getByText('Uurneerslag - eens per 1000 jaar', { exact: true }).first().click();
+  await page.getByRole('textbox').nth(1).click();
+  await page.getByText('2100 laagste scenario', { exact: true }).first().click();
 
   box = (await page.locator('.image-Zeespiegelstijging').boundingBox())!;
   await page.mouse.click(box.x + box.width / 2, box.y + box.height - 5);
